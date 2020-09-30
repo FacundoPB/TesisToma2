f8811e2eb6b2d19618e41cc4de45ac98ab2784ae
FacundoPB <53498793+FacundoPB@users.noreply.github.com>
2020-09-30 18:21:14 -0300

    Comit Inicial

---
+++ Assets/Scripts/Patrullar.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrullar : MonoBehaviour
+{
+
+    public Transform[] waypoints;
+    private int idx;
+    public float velocidad;
+    public float espera;
+    private Coroutine vigilarCorrutina;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vigilarCorrutina == null)
+        {
+            vigilarCorrutina = StartCoroutine(Vigilar());
+        }
+        
+
+    }
+    IEnumerator Vigilar()
+    {
+        Debug.Log("1");
+        if (waypoints[idx].position != transform.position)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(waypoints[idx].position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, velocidad * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[idx].position, velocidad * Time.deltaTime);
+            //yield return new WaitForSeconds(espera);
+            
+            Debug.Log("2");
+        }
+        else
+        {
+            //idx++;
+            Debug.Log("3");
+            if (idx < waypoints.Length - 1)
+            {
+                for (int i = 0; i < waypoints.Length; i++)
+                {
+                    Transform tmp = waypoints[i];
+                    int r = Random.Range(i, waypoints.Length);
+                    waypoints[i] = waypoints[r];
+                    waypoints[r] = tmp;
+
+                    yield return new WaitForSeconds(espera);
+                    vigilarCorrutina = null;
+
+                }
+
+            }
+            else
+            {
+                idx = 0;
+                Debug.Log("4");
+            }
+            
+
+        }
+
+        //yield return new WaitForSeconds(espera);
+
+
+
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rbPlayer;
+    public Camera cam;
+    Vector2 movement;
+    Vector2 worldPos;
+    public int maxhealthPlayer = 200;
+    public int currentHealth;
+    public GameObject deathEffect;
+    //public HealthBar healthBar;
+
+
+    // Update is called once per frame
+    private void Start()
+    {
+        /*currentHealth = maxhealthPlayer;
+        healthBar.SetMaxHealth(maxhealthPlayer);*/
+    }
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos = cam.ScreenToViewportPoint(Input.mousePosition);
+
+
+
+        //Debug.Log(worldPos);
+        //Debug.Log(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rbPlayer.MovePosition(rbPlayer.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = worldPos - rbPlayer.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x)*Mathf.Rad2Deg-90f;
+        rbPlayer.rotation = angle;
+        
+        //transform.rotation = Quaternion.LookRotation(lookDir);
+        
+
+
+    }
+   /* public void PlayerTakeDamage(int enemyDamage)
+    {
+        currentHealth -= enemyDamage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(effect, 4f);
+    }*/
+}
